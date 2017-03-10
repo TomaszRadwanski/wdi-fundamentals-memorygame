aeb4a85f969588ba5a9460f8053a9f0f64ed801d
Tomasz Radwanski <tomaszrad@Tomaszs-MacBook-Air.local>
2017-03-10 13:34:35 +0100

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,27 @@
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+*/
+/*
+if (cardTwo === cardFour) {
+alert('Sorry, try again.')
+} else if (cardOne === cardTwo) {
+alert('You found a match!')
+} else if (cardThree === cardFour) {
+}
+*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var gameBoard = document.getElementById('game-Board');
+
+function createCards() {
+for (var i=0; i<cards.length; i++){
+    var cardElement = document.createElement('div');
+     cardElement.className = 'card';
+     gameBoard.appendChild(cardElement);
+    }
+}
+createCards();
