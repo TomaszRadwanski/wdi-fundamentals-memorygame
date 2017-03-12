821fda42a4f5bbd772c42def8dce4b835bf5416a
Tomasz Radwanski <tomaszrad@Tomaszs-MacBook-Air.local>
2017-03-12 17:17:11 +0100

    done

--- starter-code/main.js
+++ starter-code/main.js
@@ -17,11 +17,38 @@ var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 var gameBoard = document.getElementById('game-Board');
 
-function createCards() {
-for (var i=0; i<cards.length; i++){
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
     var cardElement = document.createElement('div');
-     cardElement.className = 'card';
-     gameBoard.appendChild(cardElement);
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    gameBoard.appendChild(cardElement);
+    gameBoard.appendChild(cardElement);
+  }
+
+}
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='KOS.png'>"; 
+	} else {
+		this.innerHTML = "<img src='QOS.png'>"; 
+	}
+    if (cardsInPlay.length === 2) {
+    	isMatch(cardsInPlay);
+    	cardsInPlay = [];
     }
 }
-createCards();
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } 
+  else {
+    alert("Sorry, try again.");
+
+  }
+}
+createBoard();
